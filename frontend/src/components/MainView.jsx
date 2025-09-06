78ac4b987c38f40ed08281165fb89b7291bc8967
Shulin <shulin.js@gmail.com>
2025-09-06 23:06:01 +0530

    message handling

--- frontend/src/components/MainView.jsx
+++ frontend/src/components/MainView.jsx
@@ -1,14 +1,91 @@
 import MessageComp from "./MessageComp"
 import RoomView from "./RoomView"
-
 import "./Chat.scss"
+import { useState } from "react"
+
+
+function handleMessage(e, messages, setMessages) {
+    const target = e.target
+    const key = e.keyCode
+    const text = target.value
+
+
+    const newMessage = {
+        type: "text",
+        data: {
+            username: "Shulin",
+            avatar: "flins",
+            message: text
+        }
+    }
+
+
+    if (key === 13) {
+        // enter key pressed
+        setMessages([...messages, newMessage])
+        target.value = ""
+    }
+}
+
 
 function GeneralChat() {
+    const [messages, setMessages] = useState([
+        {
+            type: "join",
+            data: {
+                username: "Shulin"
+            }
+        },
+        {
+            type: "text",
+            data: {
+                username: "Shulin",
+                avatar: "flins",
+                message: "hello world"
+            }
+        },
+    ])
+
+
     return (
         <>
             <div className="chat">
                 <div className="chat-view">
-                    <MessageComp
+                    {
+                        messages.map((message, i) => {
+                            return (
+                                <MessageComp
+                                    type={message.type}
+                                    data={message.data}
+                                    key={i}
+                                />
+                            )
+                        })
+                        // messages.filter((message, i) => {
+                        //     if (message.type === "join") {
+                        //         return (
+                        //             <MessageComp
+                        //                 type="join"
+                        //                 data={{
+                        //                     username: "Shulin"
+                        //                 }}
+                        //             />
+                        //         )
+                        //     } else {
+                        //         return (
+                        //             <MessageComp
+                        //                 type="text"
+                        //                 data={{
+                        //                     username: "Shulin",
+                        //                     avatar: "flins",
+                        //                     message: "hello world"
+                        //                 }}
+                        //             />
+                        //         )
+                        //     }
+                        // })
+                    }
+                    {/* <MessageComp
                         type="join"
                         data={{
                             username: "Shulin"
@@ -21,10 +98,15 @@ function GeneralChat() {
                             avatar: "flins",
                             message: "hello world"
                         }}
-                    />
+                    /> */}
                 </div>
             </div>
-            <input type="text" placeholder="Message #GENERAL" className="chat-input" />
+            <input
+                type="text"
+                placeholder="Message #GENERAL"
+                className="chat-input"
+                onKeyDown={e => handleMessage(e, messages, setMessages)}
+            />
             {/* <textarea className="chat-input" style={{resize: "none"}} placeholder="Message #GENERAL" ></textarea> */}
         </>
     )
